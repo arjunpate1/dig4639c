08f683752ba9e199366b7c3277e532a0b3ac4329
Arjun Patel <40706971+arjunpate1@users.noreply.github.com>
2019-02-20 15:33:11 -0500

    Lab 3

--- lab3/screens/HomeScreen.js
+++ lab3/screens/HomeScreen.js
@@ -4,65 +4,68 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
+  FlatList,
   Text,
   TouchableOpacity,
   View,
+  ImageBackground
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+
+import { Detail } from '../screens/Detail';
+import { createStackNavigator } from 'react-navigation';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
+  _gotoScreen = (key) => {
+    console.log("Going to " + key);
+}
+
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+     const {navigate} = this.props.navigation;
+     return (
+       <ImageBackground source={require('../assets/images/bg.jpg')} imageStyle={{resizeMode: 'cover'}} style={styles.backgroundImg}>
+       <View style={styles.container}>
+         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+           <View style={styles.getStartedContainer}>
+             <Text style={styles.getStartedText}>Fresh Kicks</Text>
+             <FlatList
+              data={[{key: 'adidasnmd',
+                      image: require('../assets/images/adidas.jpg'),
+                      title: 'NMD Runner',
+                      author:'by Adidas'},
+                     {key:'nikeaf1',
+                      image: require('../assets/images/nike.jpg'),
+                      title:'Air Force 1',
+                      author:'by Nike'},
+                      {key: 'ctconverse',
+                       image: require('../assets/images/converse.jpg'),
+                       title:'Chuck Taylors',
+                       author:'by Converse'},
+                       {key: 'yeezy',
+                        image: require('../assets/images/yeezy.jpg'),
+                        title:'Yeezy Boost 350',
+                        author:'by Adidas & Kanye West'}
+                    ]}
+              keyExtractor={this._keyExtractor}
+               renderItem={({item}) => <TouchableOpacity onPress={() => navigate("Detail",{ bunnyName:item.key, bunnyImage:item.image, bunnyTitle:item.title, bunnyAuthor:item.author })}>
+                 <Image source={item.image} style={{width:200,height:200}} />
+                 <Text style={styles.imageTitle}>{item.title}</Text>
+                 <Text style={styles.imageAuthor}>{item.author}</Text>
+               </TouchableOpacity>}
+             />
+           </View>
+         </ScrollView>
+       </View>
+       </ImageBackground>
+     );
+   }
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
 
   _maybeRenderDevelopmentModeWarning() {
     if (__DEV__) {
@@ -101,7 +104,15 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  imageTitle:{
+    textAlign: 'center',
+    fontSize: 16,
+    paddingTop:5,
+  },
+  imageAuthor:{
+    textAlign: 'center',
+    marginBottom: 20,
   },
   developmentModeText: {
     marginBottom: 20,
@@ -141,10 +152,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 4,
   },
   getStartedText: {
-    fontSize: 17,
+    fontSize: 30,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
+    paddingBottom:15,
+    paddingTop:15,
+    marginTop: 50,
+    marginBottom:50,
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -184,5 +199,9 @@ const styles = StyleSheet.create({
   helpLinkText: {
     fontSize: 14,
     color: '#2e78b7',
+  },
+  backgroundImg: {
+    width: '100%',
+    height: '100%',
   },
 });
